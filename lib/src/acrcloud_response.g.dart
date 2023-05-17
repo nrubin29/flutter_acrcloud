30c237c84af52c0d2fa52913ea97fd4a4cf48165
nrubin29 <nrubin29@gmail.com>
2023-05-16 19:19:04 -0700

    Upgraded dependencies

--- lib/src/acrcloud_response.g.dart
+++ lib/src/acrcloud_response.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of flutter_acrcloud;
+part of 'acrcloud_response.dart';
 
 // **************************************************************************
 // JsonSerializableGenerator
